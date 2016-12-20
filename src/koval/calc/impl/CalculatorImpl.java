8ea5860d14ea36bc34f9a16e39d6126fd966efeb
Vlad <vlad_koval_96@mail.ru>
2016-12-20 22:36:18 +0200

    new calc impl

---
+++ src/koval/calc/impl/CalculatorImpl.java
@@ -0,0 +1,17 @@
+package koval.calc.impl;
+
+import koval.calc.Calculator;
+
+
+public class CalculatorImpl implements Calculator {
+
+    @Override
+    public int add(int a, int b) {
+        return a + b;
+    }
+
+    @Override
+    public int minus(int a, int b) {
+        return a - b;
+    }
+}
