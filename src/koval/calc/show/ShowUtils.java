ddd899d7518c0859234d5c8917cc8e29e8642e72
Vlad <vlad_koval_96@mail.ru>
2016-12-20 23:07:19 +0200

    1

--- src/koval/calc/show/ShowUtils.java
+++ src/koval/calc/show/ShowUtils.java
@@ -5,12 +5,22 @@ import koval.calc.Calculator;
 public class ShowUtils {
 
     public static void show(Calculator calculator) {
+        showAdd(calculator);
+        showMinus(calculator);
+        showMul(calculator);
+    }
+
+    private static void showAdd(Calculator calculator) {
         System.out.print("2 + 2 = ");
         System.out.println(calculator.add(2, 2));
+    }
 
+    private static void showMinus(Calculator calculator) {
         System.out.print("5 - 6 = ");
         System.out.println(calculator.minus(5, 6));
+    }
 
+    private static void showMul(Calculator calculator) {
         System.out.print("4 * 2 = ");
         System.out.println(calculator.mul(4, 2));
     }
