f99c69aa81828fd2c0af7134b84052b1d965ebe2
Vlad <vlad_koval_96@mail.ru>
2016-12-20 23:00:53 +0200

    create show utils class

--- src/koval/Demo.java
+++ src/koval/Demo.java
@@ -2,13 +2,12 @@ package koval;
 
 import koval.calc.Calculator;
 import koval.calc.impl.CalculatorImpl;
+import koval.calc.show.ShowUtils;
 
 public class Demo {
 
     public static void main(String[] args) {
         Calculator calculator = new CalculatorImpl();
-        System.out.println(calculator.add(4, 7));
-        System.out.println(calculator.minus(7, 1));
-        System.out.println(calculator.mul(3, 3));
+        ShowUtils.show(calculator);
     }
 }
